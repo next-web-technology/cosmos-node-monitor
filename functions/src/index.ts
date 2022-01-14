7b7358c2257c9b0af11e82af4181d223ab7a0c1a
YasunoriMATSUOKA <yasunori.matsuoka@gmail.com>
2022-01-14 13:11:11 +0900

    feat: functions to get validators node list periodically

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,14 +4,17 @@ import {
   getCosmosNodeInfo,
   getCosmosNodeLatestBlock,
   getCosmosNodeSyncStatus,
+  getCosmosStakingValidators,
 } from "./cosmos/cosmos.service";
 import { getNodes, setNode } from "./firestore/nodes/node.service";
+import { setValidator } from "./firestore/validators/validator.service";
 import { deleteJob, setJob } from "./firestore/jobs/job.service";
 import { Node } from "./firestore/nodes/node.model";
 import {
   convertCosmosNodeInfoToNodeInfo,
   convertCosmosNodeLatestBlockToNodeLatestBlock,
   convertCosmosNodeSyncStatusToNodeSyncStatus,
+  convertCosmosStakingValidatorsToValidators,
   convertDomainToRestApiUrl,
   convertNodeToInitialJob,
 } from "./utils/converter";
@@ -108,4 +111,57 @@ export const executeJobOnCreateJob = functions
     await setNode(node);
     await deleteJob(domain);
     functions.logger.info(domain);
+  });
+
+export const triggerJobsToMonitorValidatorsExecutedPeriodically = functions
+  .runWith({
+    timeoutSeconds: 60,
+    memory: "128MB",
+  })
+  .region("asia-northeast1")
+  .pubsub.schedule("*/5 * * * *")
+  .onRun(async () => {
+    try {
+      const nodes = await getNodes();
+      functions.logger.info(nodes);
+      const restApiEnabledNodes = nodes.filter(
+        (node) => node.restApiHttpEnabled && node.restApiHttp
+      );
+      functions.logger.debug(restApiEnabledNodes);
+      const randomSelectedNodeIndex = Math.floor(
+        Math.random() * (nodes.length - 1)
+      );
+      functions.logger.debug(randomSelectedNodeIndex);
+      const randomSelectedNode = restApiEnabledNodes[randomSelectedNodeIndex];
+      functions.logger.debug(randomSelectedNode);
+      const randomSelectedDomain = randomSelectedNode.domain;
+      functions.logger.debug(randomSelectedDomain);
+
+      const chainId: string = functions.config().cosmos.chain_id;
+      functions.logger.debug(chainId);
+
+      const cosmosValidators = await getCosmosStakingValidators(
+        randomSelectedDomain,
+        "http",
+        chainId
+      );
+      functions.logger.debug(cosmosValidators);
+      const validators =
+        convertCosmosStakingValidatorsToValidators(cosmosValidators);
+      functions.logger.debug(validators);
+
+      await Promise.all(
+        validators.map(async (validator) => {
+          try {
+            return await setValidator(validator);
+          } catch (error) {
+            functions.logger.error(error);
+            return;
+          }
+        })
+      );
+      functions.logger.info(validators);
+    } catch (error) {
+      functions.logger.error(error);
+    }
   });
